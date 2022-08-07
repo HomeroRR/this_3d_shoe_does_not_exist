16d4c7f9db9d7b7f19cff65505e494a22808d976
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2022-08-07 16:34:02 -0700

    feat(MainBanner): Change main header's typography

--- client/src/components/banner/MainBanner.tsx
+++ client/src/components/banner/MainBanner.tsx
@@ -1,5 +1,13 @@
+import { Box, Typography } from "@mui/material";
+
 function MainBanner(): JSX.Element {
-  return <div>GAN-generated 3D shoes</div>;
+  return (
+    <Box my={5}>
+      <Typography variant="h4" component="h1" style={{ textAlign: "center" }}>
+        GAN-generated 3D shoes
+      </Typography>
+    </Box>
+  );
 }
 
 export default MainBanner;
