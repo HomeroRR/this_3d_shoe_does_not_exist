c245b408f436baf459ea7c931795b3b1f0b385c1
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2022-10-12 14:46:00 -0700

    feat(MainBanner): Add logo, bgcolor and subheader

--- client/src/components/banner/MainBanner.tsx
+++ client/src/components/banner/MainBanner.tsx
@@ -1,11 +1,38 @@
-import { Box, Typography } from "@mui/material";
+import Image from "next/image";
+import { Box, Grid, Stack, Typography } from "@mui/material";
+import logoImg from "../../../public/img/logo.svg";
 
 function MainBanner(): JSX.Element {
   return (
-    <Box my={5}>
-      <Typography variant="h4" component="h1" style={{ textAlign: "center" }}>
-        GAN-generated 3D shoes
-      </Typography>
+    <Box py={5} sx={{ bgcolor: "primary.dark" }}>
+      <Grid container justifyContent="center" alignItems="center">
+        <Grid item>
+          <Image
+            src={logoImg}
+            width={100}
+            height={100}
+            alt="This 3d shoe doesn't exist logo: Calligraphy S in the form of a shoe."
+          />
+        </Grid>
+        <Grid item>
+          <Stack>
+            <Typography
+              variant="h4"
+              component="h1"
+              style={{ textAlign: "center", color: "white" }}
+            >
+              This 3D shoe doesn&apos;t exist
+            </Typography>
+            <Typography
+              variant="h5"
+              component="h1"
+              style={{ textAlign: "center", color: "white" }}
+            >
+              GAN-generated 3D shoes
+            </Typography>
+          </Stack>
+        </Grid>
+      </Grid>
     </Box>
   );
 }
